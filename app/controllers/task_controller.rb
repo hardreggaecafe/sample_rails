f59b383dea1ca48b55a40bd50e0d6905e7d2b0de
hardreggaecafe <takamizawa@frembassy.jp>
2023-04-26 21:17:00 +0900

    create update

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -6,4 +6,34 @@ class TaskController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save!
+      redirect_to @task, flash: { notice: 'タスクが作成されました' }
+    else
+      redirect_to new_task_path, flash: { error: '記事の作成に失敗しました' }
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task, flash: { notice: 'タスクが変更されました' }
+    else
+      redirect_to new_task_path, flash: { error: '記事の変更に失敗しました' }
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :status, :duedate, :label)
+  end
 end
