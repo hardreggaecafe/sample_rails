151d7699b2d3757fad25745eccc2740220bccee3
hardreggaecafe <takamizawa@frembassy.jp>
2023-04-30 19:55:11 +0900

    Search

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   enum :status, { created: 0, ready: 1, in_progress: 2, pending: 3, done: 4, canceled: 5 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "duedate", "id", "label", "name", "status", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
