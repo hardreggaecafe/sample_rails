e50019437b355a3b022148e9e7f09c207991bc29
hardreggaecafe <takamizawa@frembassy.jp>
2023-04-30 16:41:27 +0900

    Benchmark with index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_042450) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_070536) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_042450) do
     t.string "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end
