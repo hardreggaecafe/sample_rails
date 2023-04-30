6ec7ed933f5c6dbbc64eaa715ddedc79735ab4c0
hardreggaecafe <takamizawa@frembassy.jp>
2023-04-30 20:25:54 +0900

    Cast error

--- db/migrate/20230430042450_change_datatype_status.rb
+++ db/migrate/20230430042450_change_datatype_status.rb
@@ -1,9 +1,10 @@
 class ChangeDatatypeStatus < ActiveRecord::Migration[7.0]
   def change
     if Rails.env.development?
-      change_column :tasks, :status, integer
+      change_column :tasks, :status, :integer
     else
-      change_column :tasks, :status, 'numeric USING CAST(status AS numeric)'
+      remove_column :tasks, :status
+      add_column :tasks, :status, :integer, after: :description
     end
   end
 end
