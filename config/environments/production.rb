3f68a47f11559db5344ff351dcd312ebdb2630ba
hardreggaecafe <takamizawa@frembassy.jp>
2023-05-17 18:08:38 +0900

    mail error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_rails_production"
 
+  config.action_mailer.default_url_options = { host: 'sample-rails.onrender.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
